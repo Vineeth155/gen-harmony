99d75c8c532f59884664f80519e43cf3ca2a509f
Vineeth155 <jpvineeth15@gmail.com>
2024-12-28 16:18:50 +0530

    feat: sanity implementation to store audio files and use that link to add in firestore music collection

---
+++ studio-gen-harmony/schemaTypes/audio.js
@@ -0,0 +1,38 @@
+// schemas/audio.js
+export const audio = {
+  name: 'audio',
+  title: 'Audio',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'audioUrl',
+      title: 'Audio URL',
+      type: 'url', // You can store the URL of the uploaded audio here
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'generatedAt',
+      title: 'Generated At',
+      type: 'datetime',
+    },
+    {
+      name: 'generationTime',
+      title: 'Generation Time (seconds)',
+      type: 'number',
+    },
+    {
+      name: 'userId',
+      title: 'User ID',
+      type: 'string',
+    },
+  ],
+}
---
+++ studio-gen-harmony/schemaTypes/index.js
@@ -0,0 +1,3 @@
+import {audio} from './audio'
+
+export const schemaTypes = [audio]
