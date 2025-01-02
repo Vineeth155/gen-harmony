6ed33c3b2181ce126a18614afb435c44bf7a9af9
Vineeth155 <jpvineeth15@gmail.com>
2025-01-02 15:33:42 +0530

    fix: wrapping userParams in suspense

--- app/tracks/page.jsx
+++ app/tracks/page.jsx
@@ -1,13 +1,23 @@
 "use client";
 
 import MusicListLazy from "@/components/musicList/musicListLazy";
-import React from "react";
+import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
+
+function ListWithParams() {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+
+  return <MusicListLazy genreFilter={genre} />;
+}
 
 function Page() {
   return (
     <div className="mt-32 flex justify-center items-center flex-col">
       <h1 className="text-5xl pb-8">All Tracks</h1>
-      <MusicListLazy />
+      <Suspense fallback="loading....">
+        <ListWithParams />
+      </Suspense>
     </div>
   );
 }
--- components/musicList/musicListLazy.jsx
+++ components/musicList/musicListLazy.jsx
@@ -13,12 +13,8 @@ import { db } from "@/app/firebase";
 import { AudioPlayer } from "react-audio-play";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
 
-const MusicListLazy = () => {
-  const searchParams = useSearchParams();
-  const genreFilter = searchParams.get("genre");
-
+const MusicListLazy = ({ genreFilter = "" }) => {
   const router = useRouter();
 
   const [musicList, setMusicList] = useState(null);
